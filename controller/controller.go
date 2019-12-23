85ee06c84a3db0709b758c1ef23c296dd9ea2c08
Rusinov Yury <rusinovyury@gmail.com>
2019-12-23 11:10:15 +0600

    1

--- controller/controller.go
+++ controller/controller.go
@@ -79,9 +79,9 @@ func Start(context *extcon.ExtContext, rq chan int, ans chan string) {
 	}
 	count := 0
 	for rows.Next() {
-		// if count > 501 {
-		// 	break
-		// }
+		if count > 501 {
+			break
+		}
 		dev := new(device.Device)
 		rows.Scan(&dev.ID)
 		dev.Controller = getController(dev.ID, rq, ans)
