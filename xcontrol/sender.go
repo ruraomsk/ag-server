da3056174933f28d7031ff9908b8c34ae9c4c065
Rusinov Yury <rusinovyury@gmail.com>
2023-01-18 14:21:34 +0300

    Исправил ХТ

--- xcontrol/sender.go
+++ xcontrol/sender.go
@@ -10,39 +10,45 @@ import (
 )
 
 func sender() {
-	soc, err := net.Dial("tcp", setup.Set.XCtrl.FullHost)
-	if err != nil {
-		logger.Error.Printf("Sender Соединение с сервером команд %s", err.Error())
-		return
-	}
-	logger.Info.Printf("Sender started...")
-	_, err = soc.Write([]byte("0\n"))
-	if err != nil {
-		logger.Error.Printf("Передача keep alive на сервер команд %s", err.Error())
-		return
-	}
-	alive := time.NewTicker(1 * time.Minute)
 	for {
-		select {
-		case <-alive.C:
-			_, err = soc.Write([]byte("0\n"))
-			if err != nil {
-				logger.Error.Printf("Передача keep alive на сервер команд %s", err.Error())
-				return
-			}
-		case cmd := <-commARM:
-			//logger.Debug.Printf("Команда %v",cmd)
-			c, err := json.Marshal(cmd)
-			if err != nil {
-				logger.Error.Printf("Конвертация команды %v %s", cmd, err.Error())
-				return
-			}
-			c = append(c, '\n')
-			_, err = soc.Write(c)
-			if err != nil {
-				logger.Error.Printf("Передача %s на сервер команд %s", string(c), err.Error())
-				return
+		soc, err := net.Dial("tcp", setup.Set.XCtrl.FullHost)
+		if err != nil {
+			logger.Error.Printf("Sender Соединение с сервером команд %s", err.Error())
+			continue
+		}
+		logger.Info.Printf("Sender started...")
+		_, err = soc.Write([]byte("0\n"))
+		if err != nil {
+			logger.Error.Printf("Передача keep alive на сервер команд %s", err.Error())
+			soc.Close()
+			continue
+		}
+		alive := time.NewTicker(1 * time.Minute)
+		for {
+			select {
+			case <-alive.C:
+				_, err = soc.Write([]byte("0\n"))
+				if err != nil {
+					logger.Error.Printf("Передача keep alive на сервер команд %s", err.Error())
+					soc.Close()
+					break
+				}
+			case cmd := <-commARM:
+				//logger.Debug.Printf("Команда %v",cmd)
+				c, err := json.Marshal(cmd)
+				if err != nil {
+					logger.Error.Printf("Конвертация команды %v %s", cmd, err.Error())
+					continue
+				}
+				c = append(c, '\n')
+				_, err = soc.Write(c)
+				if err != nil {
+					logger.Error.Printf("Передача %s на сервер команд %s", string(c), err.Error())
+					soc.Close()
+					break
+				}
 			}
 		}
+
 	}
 }
