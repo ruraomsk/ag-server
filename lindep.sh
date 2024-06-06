0280b2ebf9402a5b0848891a28fd864c2922fcd3
Rusinov Yury <rusinovyury@gmail.com>
2024-06-06 09:28:24 +0300

    Ghbdrf

--- lindep.sh
+++ lindep.sh
@@ -5,11 +5,4 @@ if [ $? -ne 0 ]; then
 	echo 'An error has occurred! Aborting the script execution...'
 	exit 1
 fi
-# FILE=/home/rura/mnt/Linux/asud/cmd/ag-server
-# if [ -f "$FILE" ]; then
-#     echo "Mounted the server drive"
-# else
-#     echo "Mounting the server drive"
-#     sudo mount.cifs -o username=root,password=162747 //192.168.115.23/asdu /home/rura/mnt/Linux
-# fi
-# sudo cp ag-server /home/rura/mnt/Linux/asud/cmd
+cp ag-server ~/tula/asud/cmd/
